ebb0a757d9521b7af08c456dfb74ebf63c9f9371
Isabel Riemann <isabel.riemann@scoop-software.de>
2019-11-15 08:31:38 +0100

    fix mailing

--- app/src/main/java/com/example/osp/Activity_TicketDetail.java
+++ app/src/main/java/com/example/osp/Activity_TicketDetail.java
@@ -2,6 +2,7 @@ package com.example.osp;
 
 import android.os.Bundle;
 
+import com.example.osp.mailing.MailingTask;
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
 import com.google.android.material.snackbar.Snackbar;
 
@@ -9,6 +10,10 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
 
 import android.view.View;
+import android.widget.Button;
+
+import java.util.Arrays;
+import java.util.List;
 
 public class Activity_TicketDetail extends AppCompatActivity {
 
@@ -19,6 +24,21 @@ public class Activity_TicketDetail extends AppCompatActivity {
         Toolbar toolbar = findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
-    }
 
+        final Button send = findViewById(R.id.button_saveticket);
+        send.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String fromEmail = "gso.raumbetreuer.app@gmail.com";
+                String fromPassword = "Raumbetreuer";
+                String toEmails = "riemann-isabel@gmx.de";
+                List toEmailList = Arrays.asList(toEmails
+                        .split("\\s*,\\s*"));
+                String emailSubject = "TICKET FATALER FEHLER :o";
+                String emailBody = "Sorry, not sorry";
+                new MailingTask().execute(fromEmail,
+                        fromPassword, toEmailList, emailSubject, emailBody);
+            }
+        });
+    }
 }
